d7d38f1476950cee278b3ab090ff4a15597babc0
James Sterritt <james.sterritt.strategy@gmail.com>
2015-10-26 19:53:11 -0400

    Intial Project Load
    My Unity 10.1.2015 Class Build

---
+++ Unity Project 10.1/Assets/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player") {
+			GetComponent<AudioSource>().Play();
+			StartCoroutine(DestroyThis());
+		}
+	}
+	
+	IEnumerator DestroyThis(){
+		yield return new WaitForSeconds(0.5f);
+		Destroy(gameObject);
+	}
+
+
+	}
